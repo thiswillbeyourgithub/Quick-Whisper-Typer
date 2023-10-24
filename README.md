5536fbd7b0b149667084697a220db995b87c84da
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-10-24 12:19:35 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Yad Quick Microphone
+Super simple zsh script to start recording sound, send it to whisper then have it type for you in a field.
+
+## Features
+* Automatically stops if you move the mouse too much. You are supposed to launch the script, move the mouse to the field you want to type int, then press Escape to exit the yad window. This will then trigger the whisper code.
+* Input argmuent allows to choose the language
+* One can quickly use openai chatgpt to correct texts like that, including on the go translation etc
+
+## How to
+* Put your OpenAI api key in a file called API_KEY.txt
+* *optional: add a keyboard shortcut to call this script. See my i3 bindings below.*
+* launch the script
+* the recording starts instantly
+* move quickly your mouse to select the field you want to type in
+* after 1s a minimalist yad window appears and takes focus away from the field
+* press Escape to exit the yad window, this will trigger whisper
+* when whisper is done, xdotool will type for you (every few keystrokes, the script will check that your mouse has not moved to avoid launching random shortcuts into other apps)
+
+### i3 bindings
+```
+mode "$mode_launch_microphone" {
+    bindsym f exec /PATH/TO/Yad_Quick_Microphone.sh fr, mode "default
+    bindsym e exec /PATH/TO/Yad_Quick_Microphone.sh en, mode "default"
+
+    bindsym Return mode "default"
+    bindsym Escape mode "default"
+    bindsym $alt+shift+r mode "default"
+    }
+
+```
---
+++ Yad_Quick_Microphone.sh
@@ -0,0 +1,110 @@
+#!/usr/bin/zsh
+
+# set -euxo pipefail
+
+LANG=$1
+allowed_langs=("fr" "en")
+if [[ -z "$LANG" || ! " ${allowed_langs[@]}  " =~ " $LANG " ]]
+then
+    echo "Invalid lang $LANG"
+    exit 1
+fi
+
+FILE="/tmp/audio_recording_$(date +%s).mp3"
+cd "$(dirname "$0")"
+export OPENAI_API_KEY=$(cat ./API_KEY.txt)
+start_time=$(date +%s)
+min_duration=1
+max_distance=10
+
+record() {
+    echo "Recording $1"
+    rec -r 44000 -c 1 -b 16 "$1" &
+}
+type_input() {
+    xdotool type --delay 0 "$1" 2>/dev/null
+}
+check_mouse_movement() {
+    current_pos=$(xdotool getmouselocation --shell 2>/dev/null) 2>/dev/null
+
+    # Calculate the distance moved by the mouse
+    distance=$(echo "sqrt((${current_pos%X}-$initial_pos_X)^2 + (${current_pos%Y}-$initial_pos_Y)^2)" | bc) 2>/dev/null
+
+    # Check if the distance moved exceeds a specific amount
+    if (( $(echo "$distance > $max_distance" | bc -l) )); then
+        echo "Mouse movement detected. Stopping..."
+        exit 1
+    fi
+}
+
+
+# kill just in case was already running
+killall rec >/dev/null 2>&1
+
+# start recording
+record $FILE
+
+# give time to the user to position the cursor
+sleep 1
+
+# create a form, will keep going after exiting
+input=$(yad \
+    --form \
+    --title "Yad Sound Recorder" \
+    --on-top \
+    --button="STOP!gtk-media-stop":0
+    )
+    # --close-on-unfocus
+    #--accel="r":"./toggle_recording.sh \"$FILE\""
+    #--button="Record!gtk-media-record":"./toggle_recording.sh \"$FILE\"" \
+
+echo "Exit yad: $input"
+
+# kill the recording
+killall rec >/dev/null 2>&1
+echo "Done recording"
+
+end_time=$(date +%s)
+duration=$((end_time - start_time))
+echo "Duration $duration"
+
+if (( duration < min_duration ))
+then
+    echo "Recording too short, exiting without calling whisper."
+    sleep 1
+    echo ""
+    exit 0
+fi
+
+
+# echo "playing file"
+# mplayer $FILE
+
+initial_pos=$(xdotool getmouselocation --shell)
+
+echo "Calling whisper"
+text=$(openai api audio.transcribe --model whisper-1 --response-format text --temperature 0 -f $FILE  --language $LANG --prompt "Note vocale pour mon assistant : ")
+echo "Whisper text: $text"
+
+# echo "Calling chatgpt"
+# text=$(echo "$text" | llm "traduit ce texte en anglais")
+# echo "ChatGPT text: $text"
+
+# make sure to use french keyboard
+setxkbmap fr
+
+i=1
+while (( i <= ${#text} ))
+do
+    # type character
+    current_char=${text[$i]}
+    type_input "$current_char"
+    (( i++ ))
+
+    # every few character check if the mouse moved
+    if (( i % 5 == 0 ))
+    then
+        check_mouse_movement
+    fi
+done
+
