ebbb36297d0008452a1aea25d221a8f8e932857d
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-11-12 14:24:56 +0100

    major overhaul

--- README.md
+++ README.md
@@ -4,16 +4,16 @@ Super simple zsh script to start recording sound, send it to whisper then have i
 ## The way this works
 1. Move your cursor to where you want the output to be.
 2. Launch the script (preferably using a keyboard shortcut).
-3. Wait for a small window to popup. If you enter text inside it will be given as instruction to ChatGPT to alter your whisper transcript. Leave empty otherwise. Press enter to close the window without moving your cursor.
+3. A small window pops up. If you enter text inside it will be given as instruction to ChatGPT to alter your whisper transcript. Leave empty otherwise. Press enter to close the window without moving your cursor.
 4. Whisper is called on the audio.
-5. If you used `--transform_clipboard` the output will be: ChatGPT transformation of the current clipboard according to the whisper transcript. If you didn't the output will be the whisper transcript.
+5. If you used `--task=transform_clipboard` the output will be: ChatGPT transformation of the current clipboard according to the whisper transcript. If you didn't the output will be the whisper transcript.
 6. If you entered text in the small window, it will be given as instruction to ChatGPT to further transform the output (e.g. "Translate to English")
 7. The current clipboard is stored. The output text is copied. `xdotool` sends `ctrl+v` (press shift if in a console!) to add the output to where your cursor is. The clipboard is then refilled with what was previously there.
 
 ## Features
 * Choose the language via `--language`
 * Specify a whisper prompt via `--prompt`
-* If you use `--transform_clipboard`, then ChatGPT will be tasked to transform the content of your clipboard according to the instruction you told to whisper.
+* If you use `--task=transform_clipboard`, then ChatGPT will be tasked to transform the content of your clipboard according to the instruction you told to whisper.
 * Removes long silences via sox
 
 ## How to
@@ -28,7 +28,7 @@ mode "$mode_launch_microphone" {
     # enter text
     bindsym f exec /PATH/TO/Yad_Quick_Microphone.sh --language en, mode "default
     # edit clipboard
-    bindsym e exec /PATH/TO/Yad_Quick_Microphone.sh --language en --transform_clipboard, mode "default"
+    bindsym e exec /PATH/TO/Yad_Quick_Microphone.sh --language en --task=transform_clipboard, mode "default"
 
     bindsym Return mode "default"
     bindsym Escape mode "default"
--- quick_whisper_typer.sh
+++ quick_whisper_typer.sh
@@ -5,7 +5,44 @@
 # Parsing command line arguments as an array
 LANG=""
 PROMPT=""
-TRANSFORM=0
+TASK=""
+
+# pre defined prompts
+declare -A prompts
+prompts[fr]="Dictée vocale sur mon téléphone: "
+prompts[en]="Dictation on my smartphone: "
+
+# pre defined system prompts
+declare -A system_prompts
+system_prompts[vocal]="You are a helpful assistant. I am in a hurry and your answers will be played on speaker so use as few words as you can while remaining helpful and truthful."
+system_prompts[transform_clipboard]="You transform INPUT_TEXT according to an instruction. Only reply the transformed text without anything else."
+
+# pre defined vocal models
+declare -A speaker_models
+speaker_models[fr]="fr_FR-gilles-low"
+speaker_models[en]="en_US-lessac-medium"
+
+# load openai api key
+cd "$(dirname "$0")"
+export OPENAI_API_KEY=$(cat ./API_KEY.txt)
+
+# vars
+FILE="/tmp/quick_whisper_audio_$(date +%s).mp3"
+start_time=$(date +%s)
+min_duration=2  # if the recording is shorter, exit
+
+# functions
+log() {
+    echo $1
+    echo "$(date +%s) $1" >> ./texts.log
+}
+record() {
+    log "Recording $1"
+    rec -r 44000 -c 1 -b 16 "$1" &
+}
+
+
+# gather user arguments
 for arg in "$@"; do
     case "$arg" in
         -l | --language)
@@ -16,8 +53,8 @@ for arg in "$@"; do
             PROMPT="$2"
             shift 2
             ;;
-        -t | --transform_clipboard)
-            TRANSFORM=1
+        -t | --task)
+            TASK="$2"
             shift 2
             ;;
         #*)
@@ -26,55 +63,54 @@ for arg in "$@"; do
         #    ;;
     esac
 done
-echo "Will use language $LANG and prompt $PROMPT and transform $TRANSFORM "
 
 # check if the language is supplied and correct
 allowed_langs=("fr" "en")
 if [[ -z "$LANG" || ! " ${allowed_langs[@]}  " =~ " $LANG " ]]
 then
-    echo "Invalid lang $LANG"
+    echo "Invalid lang $LANG not part of $allowed_langs"
+    exit 1
+fi
+speaker_model="${speaker_models[$LANG]}"
+
+# selecting prompt based on lang
+if [[ -z "$PROMPT" ]]
+then
+    PROMPT="${prompts[$LANG]}"
+fi
+
+# checking that the task is allowed
+allowed_tasks=("transform_clipboard" "new_vocal_chat" "continue_vocal_chat" "write")
+if [[ -z "$TASK" || ! " ${allowed_tasks[@]}  " =~ " $TASK " ]]
+then
+    echo "Invalid task $TASK not part of $allowed_tasks"
     exit 1
 fi
 
-if [[ -z "$PROMPT" ]]
+# check that the clipboard is not empty
+prev_clipboard=$(xclip -o -sel clipboard)
+if [[ $TASK == "transform_clipboard" ]]
 then
-    if [[ "$LANG" == "fr" ]]
+    if [[ -z "$prev_clipboard" ]]
     then
-        PROMPT="Dictée vocale sur mon téléphone: "
-     elif [[ "$LANG" == "en" ]]
-    then
-        PROMPT="Dictation on my smartphone: "
+        log "Clipboard is empty, this is not compatible with $task"
+        exit 1
     fi
 fi
 
+# telling user that everything worked
+log "\n"
+log "\n"
+log "Will use language $LANG and prompt $PROMPT and task $TASK "
 
-# load openai api key
-cd "$(dirname "$0")"
-export OPENAI_API_KEY=$(cat ./API_KEY.txt)
-
-FILE="/tmp/audio_recording_$(date +%s).mp3"
-start_time=$(date +%s)
-min_duration=3  # if the recording is shorter, exit
-
-record() {
-    log "Recording $1"
-    rec -r 44000 -c 1 -b 16 "$1" &
-}
-
-log() {
-    echo $1
-    echo "$(date +%s) $1" >> ./texts.log
-}
-
-
-# kill just in case was already running
+# kill just in case rec was already running
 killall rec >/dev/null 2>&1
 
 # start recording
 record $FILE
 
 # give time to the user to position the cursor
-sleep 1
+# sleep 1
 
 # yad form
 instruction=$(yad \
@@ -84,7 +120,7 @@ instruction=$(yad \
     --on-top \
     --button="STOP!gtk-media-stop":0
     )
-log "\nChatGPT instruction: $instruction for task $TRANSFORM"
+log "\nChatGPT instruction: $transf_instruct for task $TASK"
 
 # kill the recording
 killall rec >/dev/null 2>&1
@@ -102,36 +138,84 @@ then
     exit 0
 fi
 
-
-# echo "playing file"
-# mplayer $FILE
-
-# removing silences longer than Xs
-rm /tmp/tmpoutput*.mp3
-sox $FILE /tmp/tmpoutput.mp3 silence 1 1 0.1% 1 1 0.1% : newfile : restart
-cat /tmp/tmpoutput*.mp3 > "/tmp/unsilenced_$FILE"
-rm /tmp/tmpout*.mp3
-$FILE="unsilenced_$FILE"
-log "Removed silence, new file is $FILE"
+# # removing silences longer than Xs
+# rm /tmp/tmpoutput*.mp3
+# sox $FILE /tmp/tmpoutput.mp3 silence 1 1 0.1% 1 1 0.1% : newfile : restart
+# cat /tmp/tmpoutput*.mp3 > "/tmp/unsilenced_$FILE"
+# rm /tmp/tmpout*.mp3
+# $FILE="unsilenced_$FILE"
+# log "Removed silence, new file is $FILE"
 
 log "Calling whisper"
 text=$(openai api audio.transcribe --model whisper-1 --response-format text --temperature 0 -f $FILE  --language $LANG --prompt "$PROMPT")
 log "Whisper transcript: $text"
 
-prev_clipboard=$(xclip -o -sel clipboard)
-
-if [[ $TRANSFORM == "1" ]]
+if [[ $TASK == "transform_clipboard" ]]
     then
-    log "Calling ChatGPT with instruction $instruction to transform the clipboard"
-    log "current keyboard: $prev_clipboard"
-    text=$(openai api chat_completions.create --model gpt-3.5-turbo-1106 -g system "You transform INPUT_TEXT according to an instruction. Only reply the transformed text without anything else." -g user "INPUT_TEXT: '$prev_clipboard'\n\nINSTRUCTION: '$text'")
-    log "ChatGPT answer after transformation: $text"
+    log "Calling ChatGPT with instruction \"$transf_instruct\" and tasked to transform the clipboard"
+    log "Previous clipboard: $prev_clipboard"
+    text=$(openai api chat_completions.create --model gpt-3.5-turbo-1106 -g system ${system_prompts[transform_clipboard]} -g user "INPUT_TEXT: '$prev_clipboard'\n\nINSTRUCTION: '$text'")
+    log "ChatGPT clipboard transformation: \"$text\""
+
+elif [[ $TASK == "new_vocal_chat" ]] || [[ $TASK == "continue_vocal_chat" ]]
+then
+
+    if [[ $TASK == "new_vocal_chat" ]]
+    then
+        VOCAL_FILE="/tmp/quick_whisper_chat_$(date +%s).txt"
+        log "Creating new vocal chat file: $VOCAL_FILE"
+        message_arg=""
+
+    else
+        VOCAL_FILE=$(\ls -t /tmp/quick_whisper_chat_* | head -1)
+        log "Reusing previous vocal chat file: $VOCAL_FILE"
+
+        # read the previous message list
+        messages=()
+        IFS="#####"
+        while IFS= read -r line; do
+            messages+=("$line")
+        done < $VOCAL_FILE
+
+        # parse the message as a long arg string
+        message_arg=""
+        for element in "${messages[@]}"; do
+            read -r -a fields <<< "$element"
+            for ((i = 0; i < ${#fields[@]}; i++)); do
+                if ((i % 2 == 0)); then
+                    role="user"
+                else
+                    role="assistant"
+                fi
+                message_arg+="-g $role \"${fields[i]}\" "
+            done
+        done
+
     fi
 
-if [[ ! -z $instruction ]]
+    answer=$(openai api chat_completions.create --model gpt-3.5-turbo-1106 -g system $vocal_system_prompt $message_arg -g "user" "$text")
+    log "ChatGPT answer to the chat: \"$answer\""
+
+    # add text and answer to the file
+    echo "\n#####\n" >> $VOCAL_FILE
+    echo $text >> $VOCAL_FILE
+    echo "\n#####\n" >> $VOCAL_FILE
+    echo $answer >> $VOCAL_FILE
+
+    # play vocal file
+    VOCAL_FILE_MP3="/tmp/quick_whisper_piper_$(date +%s).mp3"
+    log "storing vocal mp3 to $VOCAL_FILE_MP3"
+    echo "$answer" | python -m piper --model $speaker_model --output_file $VOCAL_FILE_MP3 2&>1
+    mplayer -really-quiet $VOCAL_FILE_MP3 2&>1
+    log "done playing vocal file"
+
+    exit
+fi
+
+if [[ ! -z $transf_instruct ]]
 then
-    log "Calling ChatGPT with instruction $instruction"
-    text=$(openai api chat_completions.create --model gpt-3.5-turbo-1106 -g system "You transform INPUT_TEXT according to an instruction. Only reply the transformed text without anything else." -g user "INPUT_TEXT: '$text'\n\nINSTRUCTION: '$instruction'")
+    log "Calling ChatGPT with instruction $transf_instruct"
+    text=$(openai api chat_completions.create --model gpt-3.5-turbo-1106 -g system ${system_prompts[transform_clipboard]} -g user "INPUT_TEXT: '$text'\n\nINSTRUCTION: '$transf_instruct'")
     log "ChatGPT output: $text"
 else
     log "Not using ChatGPT"
@@ -139,6 +223,7 @@ fi
 
 
 # save to all clipboard just in case
+log "using clipboard to write"
 echo "$text" | xclip -sel primary
 echo "$text" | xclip -sel secondary
 echo "$text" | xclip -sel clipoard
