bde7fd7bc5ee3a30d40f493a8c662550732de782
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-02-27 11:16:33 +0100

    add a launch script
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

---
+++ quick_whisper_typer_launcher.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+# if you use a venv:
+# cd YOUR_PATH
+# source .venv/bin/activate
+
+# # if you use a custom whisper, for example via speaches.ai:
+# export CUSTOM_WHISPER_API_KEY="YOUR KEY"
+# --custom_transcription_url="http://localhost:8001/v1/audio/transcriptions" \
+#
+# or to use deepgram:
+# --deepgram_transcription \
+
+python quick_whisper_typer.py  \
+    --voice_engine=openai \
+    --disable_voice \
+    --auto_paste \
+    --loop \
+    --verbose \
+    --sound_cleanup \
+    --loop_tasks '{"n":{"task":"new_voice_chat"}, "c": {"task":"continue_voice_chat"}, "w": {"task": "write"}, "t": {"task": "transform_clipboard"}, "s": {"extra_args": "disable_voice"}, "x": {"task": "write", "LLM_instruction": "my_instructions.txt"}, "d": {"task": "write", "LLM_instruction": "corrector.txt", "llm_model": "openai/gpt-4o-mini"}}'
